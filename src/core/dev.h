f4c0dc34654256407d11f2de5e2901136634577d
embalmer-Y <y19858120112@163.com>
2024-06-17 19:17:18 +0800

    Add the thread ctrl block struct and refine route struct.

--- src/core/dev.h
+++ src/core/dev.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <pthread.h>
 
 #include "data.h"
 
@@ -32,27 +33,40 @@ struct ability_ops {
     int (*callback)(int argc, void *argv[]);
 };
 
-struct ability_route_ability_node {
-    struct ability_route_ability_node *next;
-    struct ability_route_ability_node *prev;
+struct ability_route_ability {
+    struct ability_route_ability *next;
+    struct ability_route_ability *prev;
 
     uint16_t ability_id;
 };
 
-struct ability_route_dev_node {
-    struct ability_route_dev_node *next;
-    struct ability_route_dev_node *prev;
+struct ability_route_dev {
+    struct ability_route_dev *next;
+    struct ability_route_dev *prev;
 
     uint16_t dev_id;
-    struct ability_route_ability_node *head;
-    struct ability_route_ability_node *tail;
+    struct ability_route_ability *head;
+    struct ability_route_ability *tail;
 };
 
-struct ability_route {
+struct ability_route_ctrl_block {
     uint32_t dev_num;
     uint32_t ability_num;
-    struct ability_route_dev_node *head;
-    struct ability_route_dev_node *tail;
+    struct ability_route_dev *head;
+    struct ability_route_dev *tail;
+};
+
+struct ability_thread {
+    struct ability_thread *next;
+    struct ability_thread *prev;
+
+    pthread_t thread_id;
+};
+
+struct ability_thread_ctrl_block {
+    struct ability_thread* head;
+    struct ability_thread* tail;
+    uint16_t count;
 };
 
 struct ability {
@@ -65,7 +79,9 @@ struct ability {
     char *desc;
     enum ability_type type;
     enum ability_state state;
-    struct ability_route route;
+    struct ability_route_ctrl_block *rcb;
+    struct ability_thread_ctrl_block *tcb;
+    struct data_ctrl_block *dcb;
 
     struct ability_ops ops;
 };
@@ -74,7 +90,6 @@ struct ability_ctrl_block {
     struct ability* head;
     struct ability* tail;
     uint16_t count;
-    struct data_ctrl_block *data;
 };
 
 struct device {
