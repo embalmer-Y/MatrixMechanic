d50ec32dd533b78799da44b64b904297f51f0a36
embalmer-Y <y19858120112@163.com>
2024-06-18 18:52:08 +0800

    Add device ctrl block flow and add dev errno.

--- src/core/dev.c
+++ src/core/dev.c
@@ -3,3 +3,37 @@
 
 #include "dev.h"
 #include "common.h"
+
+int dev_init(struct device_ctrl_block *dcb)
+{
+    if (dcb == NULL) {
+        return -DEVICE_ERR_PARAMETER;
+    }
+
+    dcb->count = 0;
+
+    // TODO: Initialize main device
+
+    return 0;
+}
+
+int dev_deinit(struct device_ctrl_block *dcb)
+{
+    if (dcb == NULL) {
+        return -DEVICE_ERR_PARAMETER;
+    }
+
+    // TODO: Deinitialize main device
+
+    return 0;
+}
+
+struct device_ctrl_block *dev_alloc(void)
+{
+    return(struct device_ctrl_block *)malloc(sizeof(struct device_ctrl_block));
+}
+
+void dev_free(struct device_ctrl_block *dcb)
+{
+    free(dcb);
+}
